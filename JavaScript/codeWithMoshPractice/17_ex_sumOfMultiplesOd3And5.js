d7fc5ef85577dd5c8cc568ca33b33c759fb3caf6
heejinlee07 <heejinlee07@gmail.com>
2020-03-30 11:54:34 +0900

    forLoop practice

--- JavaScript/codeWithMoshPractice/17_ex_sumOfMultiplesOd3And5.js
+++ JavaScript/codeWithMoshPractice/17_ex_sumOfMultiplesOd3And5.js
@@ -4,12 +4,11 @@ console.log(sum(10));
 //multiple of 5: 5,10
 
 function sum(limit) {
-  let sum = 0;
-
+  let result = 0;
   for (let i = 0; i <= limit; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
-      sum += i;
+      result += i;
     }
   }
-  return sum;
+  return result;
 }
--- JavaScript/codeWithMoshPractice/19_ex_stars.js
+++ JavaScript/codeWithMoshPractice/19_ex_stars.js
@@ -1,19 +1,9 @@
 showStars(5);
 
 function showStars(rows) {
-  for (let row = 1; row <= rows; row++) {
-    let pattern = "";
-    for (let i = 0; i < row; i++) {
-      pattern += "*";
-    }
-    console.log(pattern);
+  let star = "";
+  for (let i = 0; i < rows; i++) {
+    star += "*";
+    console.log(star);
   }
-}
-
-var star = "";
-var line = 5;
-
-for (let i = 0; i < line; i++) {
-  star += "*";
-  console.log(star);
 }
